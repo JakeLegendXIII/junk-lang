994eb0d7049342252992425fd193eb33c47e4d03
Jake <jakelegendxiii@gmail.com>
2023-08-15 21:38:45 -0400

    add parser to repl

--- src/junk/repl/repl.go
+++ src/junk/repl/repl.go
@@ -5,10 +5,18 @@ import (
 	"fmt"
 	"io"
 	"junk/lexer"
-	"junk/token"
+	"junk/parser"
 )
 
 const PROMPT = ">> "
+const RACCOON_JUNK = `
+  _             _    
+  (_)_   _ _ __ | | __
+  | | | | | '_ \| |/ /
+  | | |_| | | | |   < 
+ _/ |\__,_|_| |_|_|\_\
+|__/                                                 
+`
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
@@ -20,10 +28,26 @@ func Start(in io.Reader, out io.Writer) {
 			return
 		}
 
-		line := scanner.Text()                                                 // get input
-		l := lexer.New(line)                                                   // create lexer
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() { // loop through tokens
-			fmt.Printf("%+v\n", tok) // print tokens
+		line := scanner.Text() // get input
+		l := lexer.New(line)   // create lexer
+		p := parser.New(l)
+
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, RACCOON_JUNK)
+	io.WriteString(out, "Woops! We ran into some junk here!\n")
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
