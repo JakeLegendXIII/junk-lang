a548d84fd789941718a20d6868031e508740dde2
Jake <jakelegendxiii@gmail.com>
2023-08-21 22:42:09 -0400

    added evalutor to repl

--- src/junk/repl/repl.go
+++ src/junk/repl/repl.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"junk/evaluator"
 	"junk/lexer"
 	"junk/parser"
 )
@@ -38,8 +39,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluatored := evaluator.Eval(program)
+		if evaluatored != nil {
+			io.WriteString(out, evaluatored.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
