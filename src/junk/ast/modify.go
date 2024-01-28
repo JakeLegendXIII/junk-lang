75423f3d15b40ed0f52185d5d640843f57519fd4
Jacob Kirkbride <jakelegendxiii@gmail.com>
2024-01-28 01:24:42 -0500

    Add modify

---
+++ src/junk/ast/modify.go
@@ -0,0 +1,18 @@
+package ast
+
+type ModifierFunc func(Node) Node
+
+func Modify(node Node, modifier ModifierFunc) Node {
+	switch node := node.(type) {
+
+	case *Program:
+		for i, statement := range node.Statements {
+			node.Statements[i], _ = Modify(statement, modifier).(Statement)
+		}
+
+	case *ExpressionStatement:
+		node.Expression, _ = Modify(node.Expression, modifier).(Expression)
+	}
+
+	return modifier(node)
+}
