35514db128a60a68007dbd5c7ba2044b55474272
Jacob Kirkbride <jakelegendxiii@gmail.com>
2024-02-02 23:39:05 -0500

    add macro support to repl

--- src/junk/repl/repl.go
+++ src/junk/repl/repl.go
@@ -23,6 +23,7 @@ const RACCOON_JUNK = `
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
 	env := object.NewEnvironment()
+	macroEnv := object.NewEnvironment()
 
 	for {
 		fmt.Printf(PROMPT)        // print prompt
@@ -41,7 +42,10 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program, env)
+		evaluator.DefineMacros(program, macroEnv)
+		expanded := evaluator.ExpandMacros(program, macroEnv)
+
+		evaluated := evaluator.Eval(expanded, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
